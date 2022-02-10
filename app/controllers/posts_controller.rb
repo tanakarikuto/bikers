822ff1eb7292d332c9da6e8ca81657e5769958f5
tanakarikuto <rikuto616tanaka@gmail.com>
2022-02-10 09:54:56 +0000

    9回目

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,18 @@
 class PostsController < ApplicationController
+  def index
+   @posts = @post.all
+  end
   
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = current_user.posts.new
+     if @post.save
+       redirect_to request.referer, success: 'ポストに成功しました' #←同じページにリダイレクト
+     else
+         flash.now[:danger] = "ポストに失敗しました"
+     end
+  end
 end
