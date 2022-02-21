19ec953f5306c7929b30b713eff7a592ae7dcc29
tanakarikuto <rikuto616tanaka@gmail.com>
2022-02-21 12:19:27 +0000

    14回目

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   
    protect_from_forgery with: :exception
+   
   add_flash_types :success, :info, :warning, :danger
   
   helper_method :current_user, :logged_in?
